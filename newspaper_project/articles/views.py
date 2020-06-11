665d56996f259c708743e984990b8d603d36b0d8
jkinobe <jkinobe@gmail.com>
2020-06-11 17:41:07 +0300

    populating the html pages

--- newspaper_project/articles/views.py
+++ newspaper_project/articles/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, DeleteView
-
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import  DeleteView, UpdateView
+from .models import Article
+from django.urls import reverse_lazy
 # Create your views here.
+
+class ArticleListView(ListView):
+    template_name = "article_list.html"
+    model = Article
+    
+class ArticleDetailView(DetailView):
+    template_name = "article_detail.html"
+    model = Article
+
+class ArticleUpdateView(UpdateView):
+    template_name = "article_edit.html"
+    fields = ('title', 'body')
+    model = Article
+    
+class ArticleDeleteView(DeleteView):
+    template_name = "article_delete.html"
+    model = Article
+    success_url = reverse_lazy('article_list') 
