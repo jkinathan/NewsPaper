2e8f447a17457ae5baa5d29663611a84156df909
jkinobe <jkinobe@gmail.com>
2020-06-12 15:36:55 +0300

    added Tabularinline and CommentInline in admin for comments and also related_name for comments to access to front end templates

--- newspaper_project/articles/admin.py
+++ newspaper_project/articles/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from .models import Article, Comment
 # Register your models here.
 
-admin.site.register(Article)
+class CommentInline(admin.TabularInline): # new can also use StackedInline
+    model = Comment
+    
+class ArticleAdmin(admin.ModelAdmin): # new
+    inlines = [
+    CommentInline,
+    ]
+    
+admin.site.register(Article,ArticleAdmin)
 admin.site.register(Comment)
 
--- newspaper_project/articles/models.py
+++ newspaper_project/articles/models.py
@@ -20,6 +20,7 @@ class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     comment = models.CharField(max_length=140)
     author = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+    related_name = 'comments'
     
     def __str__(self):
         return self.comment
