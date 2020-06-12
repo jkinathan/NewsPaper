dabab8cceb6af930b70853faab178dcfa281d810
jkinobe <jkinobe@gmail.com>
2020-06-12 15:53:59 +0300

    added comments in articlelist front end

--- newspaper_project/articles/models.py
+++ newspaper_project/articles/models.py
@@ -17,14 +17,14 @@ class Article(models.Model):
         return reverse('article_detail', args=[str(self.id)])
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE,related_name = 'comments',)
     comment = models.CharField(max_length=140)
     author = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
-    related_name = 'comments'
+    
     
     def __str__(self):
         return self.comment
     
     def get_absolute_url(self):
-        return reverse("article_list", kwargs={"pk": self.pk})
+        return reverse("article_list")
     
