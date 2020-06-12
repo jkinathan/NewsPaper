4a005a279471dc60a750a016dd3e15a4769a8bd9
jkinobe <jkinobe@gmail.com>
2020-06-12 15:22:56 +0300

    migrate comment and update admin.py file

--- newspaper_project/articles/models.py
+++ newspaper_project/articles/models.py
@@ -16,7 +16,7 @@ class Article(models.Model):
     def get_absolute_url(self):
         return reverse('article_detail', args=[str(self.id)])
 
-class Comment(models.Models):
+class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     comment = models.CharField(max_length=140)
     author = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
